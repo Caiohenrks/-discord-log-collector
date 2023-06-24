0a16ec5a1982dd5608b90b9840161da4ea6c1106
Caio Henrique <35731497+Caiohenrks@users.noreply.github.com>
2023-06-24 19:34:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
+import asyncio
 import discord
 from discord.ext import tasks
-from dotenv import load_dotenv
 import os
-import subprocess
-
-load_dotenv()
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
-log_file_path = r"/root/mine-server/logs/latest.log"
-channel_id = 1014947550480048178 
+log_file_path = r"/root/mine-server-fabric/logs/latest.log"
+channel_id = 1120809425582706829  # ID do canal do Discord onde as mensagens serão enviadas
 
-@tasks.loop(seconds=1) 
+@tasks.loop(seconds=1)  # Define a frequência com que o log será verificado (1 segundo neste exemplo)
 async def check_log():
-    process = subprocess.Popen(['tail', '-n', '0', '-F', log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = await asyncio.create_subprocess_exec(
+        'tail', '-n', '0', '-F', log_file_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
     while True:
-        line = process.stdout.readline().decode().strip()
+        line = await process.stdout.readline()
         if line:
             channel = client.get_channel(channel_id)
-            await channel.send(line)
+            await channel.send(line.decode().strip())
 
 @client.event
 async def on_ready():
@@ -31,7 +32,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!testebot'):
-        await message.channel.send('bot funcionando!')
+    if message.content.startswith('!bot'):
+        await message.channel.send('BOT OK!')
 
 client.run(os.getenv('TOKEN2'))
